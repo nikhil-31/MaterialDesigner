687a66619c0c5e20d15757931c9ce48162b5378b
nikhil-31 <nkl07ba@gmail.com>
2016-08-11 12:28:53 +0530

    Sliding tab layout set with viewpager and shows data accordingly

--- app/src/main/java/com/example/nikhil/materialtester/MainActivity.java
+++ app/src/main/java/com/example/nikhil/materialtester/MainActivity.java
@@ -2,17 +2,30 @@ package com.example.nikhil.materialtester;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.nikhil.materialtester.tabs.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private SlidingTabLayout mTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout),toolbar);
+
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs.setViewPager(mViewPager);
 
     }
 
@@ -65,6 +84,56 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public class MyPagerAdapter extends FragmentPagerAdapter{
+        String[] tabs;
+        public MyPagerAdapter(FragmentManager fm) {
+
+            super(fm);
+            tabs= getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = MyFragment.getInstance(position);
+
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+    public static class MyFragment extends Fragment{
+
+        private TextView textView;
+        public static MyFragment getInstance(int position){
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position",position);
+            myFragment.setArguments(args);
+            return myFragment;
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_my,container,false);
+            textView = (TextView) layout.findViewById(R.id.text_my);
+            Bundle bundle = getArguments();
+            if(bundle != null){
+                textView.setText("The page currently selected is "+bundle.getInt("position"));
+
+            }
+            return layout;
+        }
     }
 
 }
