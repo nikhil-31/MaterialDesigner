4918cc49478d0f5436a1642d33c0f626613ac10d
nikhil-31 <nkl07ba@gmail.com>
2016-08-12 21:02:46 +0530

    Volley singleton added with an image loader

--- app/src/main/java/com/example/nikhil/materialtester/network/VolleySingleton.java
+++ app/src/main/java/com/example/nikhil/materialtester/network/VolleySingleton.java
@@ -1,6 +1,10 @@
 package com.example.nikhil.materialtester.network;
 
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 import com.example.nikhil.materialtester.MyApplication;
 
@@ -11,8 +15,22 @@ public class VolleySingleton {
 
     private static VolleySingleton sInstance =null;
     private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
     private VolleySingleton(){
         mRequestQueue = Volley.newRequestQueue(MyApplication.getAppContext());
+        mImageLoader=new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
+            LruCache<String,Bitmap> cache = new LruCache<>((int)(Runtime.getRuntime().maxMemory()/1024/8));
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url,bitmap);
+            }
+        });
     }
 
     public static VolleySingleton getInstance(){
@@ -23,5 +41,8 @@ public class VolleySingleton {
     }
     public RequestQueue getRequestQueue(){
         return mRequestQueue;
+    }
+    public ImageLoader getImageLoader(){
+        return mImageLoader;
     }
 }
