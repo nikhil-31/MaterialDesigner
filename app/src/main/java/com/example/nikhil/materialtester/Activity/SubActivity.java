dbf2ed0d4dc3b454dba2daf2907a27d7f7a88fca
nikhil-31 <nkl07ba@gmail.com>
2016-08-17 23:31:07 +0530

    Changed to red color scheme

--- app/src/main/java/com/example/nikhil/materialtester/Activity/SubActivity.java
+++ app/src/main/java/com/example/nikhil/materialtester/Activity/SubActivity.java
@@ -23,41 +23,7 @@ public class SubActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ImageView poster = (ImageView) findViewById(R.id.poster_details);
-        TextView releaseDate = (TextView) findViewById(R.id.Release_write);
-        TextView rating = (TextView) findViewById(R.id.Rating_write);
-        TextView title = (TextView) findViewById(R.id.Title_write);
-        TextView overview = (TextView) findViewById(R.id.Overview_write);
-        ImageView backdrop = (ImageView) findViewById(R.id.backdrop);
 
-        Intent intent = getIntent();
-        Movie movie = intent.getParcelableExtra("PARCEL_MOVIE");
-
-        Picasso.with(this)
-                .load(movie.getPosterPath())
-                .into(poster);
-
-        Picasso.with(this)
-                .load(movie.getBackdrop())
-                .into(backdrop);
-
-        releaseDate.setText(movie.getReleaseDate());
-
-        rating.setText(movie.getVoteAverage());
-
-        title.setText(movie.getOriginalTitle());
-
-        overview.setText(movie.getOverview());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
